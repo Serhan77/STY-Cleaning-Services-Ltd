bfa5efbcef9a216b2e455bd09010c9023e0d0d16
Serhan77 <41770776+Serhan77@users.noreply.github.com>
2019-05-14 09:59:05 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-// Nav
+// Contact Form
+
+$(".input").focus(function () {
+  $(this).parent().addClass("focus");
+}).blur(function () {
+  if ($(this).val() === '')
+    $(this).parent().removeClass("focus");
+})
 
