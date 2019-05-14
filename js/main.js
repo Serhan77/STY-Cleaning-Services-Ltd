00b1d79b3a8bb65fcfb452f07ceb2726bd5fa0cb
Serhan77 <41770776+Serhan77@users.noreply.github.com>
2019-05-14 15:51:44 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,3 +7,21 @@ $(".input").focus(function () {
     $(this).parent().removeClass("focus");
 })
 
+// Accordian
+
+let accordions = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < accordions.length; i++) {
+  accordions[i].onclick = function () {
+    this.classList.toggle("is-open");
+    let content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      // accordian is open
+      content.style.maxHeight = null;
+    } else {
+      // accordian is closed
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+}
